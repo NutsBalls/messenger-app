0f81ba99c2b117d01c1591e3afe0fce50f2f452c
vlad <ieliseenko79@gmail.com>
2025-11-09 18:53:40 +0300

    msgs: add member handlers

--- messages/internal/messages/domain/requests.go
+++ messages/internal/messages/domain/requests.go
@@ -3,32 +3,37 @@ package domain
 import "github.com/google/uuid"
 
 type CreateMessageRequest struct {
-	ChatID   uuid.UUID
-	SenderID uuid.UUID
-	Content  string
+	ChatID   uuid.UUID `json:"chat_id"`
+	SenderID uuid.UUID `json:"sender_id"`
+	Content  string    `json:"content"`
 }
 
 type GetMessagesRequest struct {
-	ChatID uuid.UUID
+	ChatID uuid.UUID `json:"chat_id"`
 }
 
 type EditMessageRequest struct {
-	MessageID  uuid.UUID
-	NewContent string
+	MessageID  uuid.UUID `json:"message_id"`
+	NewContent string    `json:"new_content"`
 }
 
 type DeleteMessageRequest struct {
-	MessageID uuid.UUID
+	MessageID uuid.UUID `json:"message_id"`
 }
 
 type CreateChatRequest struct {
-	IsGroup bool
+	IsGroup bool `json:"is_group"`
 }
 
 type DeleteChatRequest struct {
-	ChatID uuid.UUID
+	ChatID uuid.UUID `json:"chat_id"`
 }
 
 type CreateGroupChat struct {
-	Name string
+	Name string `json:"name"`
+}
+
+type UserChatRequest struct {
+	ChatID uuid.UUID `json:"chat_id"`
+	UserID uuid.UUID `json:"user_id"`
 }
--- messages/internal/messages/handlers/member_handlers.go
+++ messages/internal/messages/handlers/member_handlers.go
@@ -1,1 +1,75 @@
 package handlers
+
+import (
+	"messages/internal/messages/domain"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// AddUserToChat
+func (h *MessagesHandlers) AddUserToChat(c echo.Context) error {
+	var req domain.UserChatRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, "bad request")
+	}
+
+	params := domain.UserChat(req)
+
+	if err := h.service.AddUserToChat(c.Request().Context(), params); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, "user added in chat")
+
+}
+
+// RemoveUserFromChat
+func (h *MessagesHandlers) RemoveUserFromChat(c echo.Context) error {
+	var req domain.UserChatRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, "bad request")
+	}
+
+	params := domain.UserChat(req)
+
+	if err := h.service.RemoveUserFromChat(c.Request().Context(), params); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, "user was removed from chat")
+}
+
+// GetChatMembers
+func (h *MessagesHandlers) GetChatMembers(c echo.Context) error {
+	var req domain.UserChatRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, "bad request")
+	}
+
+	members, err := h.service.GetChatMembers(c.Request().Context(), req.ChatID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, members)
+}
+
+// GetUserChats
+func (h *MessagesHandlers) GetUserChats(c echo.Context) error {
+	var req domain.UserChatRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, "bad request")
+	}
+
+	chats, err := h.service.GetUserChats(c.Request().Context(), req.UserID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, chats)
+}
